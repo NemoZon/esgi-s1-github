7b5109be56ce2c6709fe7a8280d2bec47e8f2363
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-12-18 12:57:07 +0100

    code: add project overview section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple': '#6533FF',
+        'yellow': '#FBE4AB',
+      },
+      spacing: {
+        '26px': '26px',
+        '154px': '154px',
+        '160px': '160px',
+        '258px': '258px',
+        '502px': '502px',
+      },
+      content: {
+        'arrow': '_>', 
+      }
+    },
   },
   plugins: [],
 }
